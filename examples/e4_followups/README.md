50cfe876268c0870b67806a1c510f2c1ea11f21a
Hugo Woesthuis <hugow2011@outlook.com>
2021-05-06 16:16:38 +0200

    add example 4

---
+++ examples/e4_followups/README.md
@@ -0,0 +1,18 @@
+# Example 4: Creating Follow-up messages
+
+Sometimes, responding with just one message isn't enough. For those situations we have 'follow-up' messages.
+
+These are webhook messages and are just like other messages (except you can add linked text too!).
+
+Just like InteractionResponses, you can edit and delete as you wish.
+
+# Running this example
+You can use regular `cargo build` and `cargo run` commands.
+
+To run this example:
+
+`cargo run`. Note that you'll need to edit the `PUB_KEY` constant accordingly (it will panic if you don't give a vaild key).
+You'll also need to supply a TLS certificate and it's corresponding private key (`cert.pem` and `key.pem` by default).
+
+# Useful documentation
+- [FollowupMessage](https://docs.rs/rusty_interaction/latest/rusty_interaction/types/interaction/struct.FollowupMessage.html)
---
+++ examples/e4_followups/src/main.rs
@@ -0,0 +1,70 @@
+#[macro_use] extern crate rusty_interaction;
+
+use rusty_interaction::handler::InteractionHandler;
+use rusty_interaction::types::interaction::*;
+
+// Used for getting TLS to work
+use rustls::internal::pemfile::{certs, pkcs8_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
+use std::fs::File;
+use std::io::BufReader;
+
+use std::time::Duration;
+use async_std::task;
+
+
+const PUB_KEY: &str = "57028473720a7c1d4666132a68007f0902034a13c43cc2c1658b10b5fc754311"; 
+
+
+#[slash_command]
+async fn test(ctx: Context) -> InteractionResponse{
+    
+    // Wait for three secs
+    task::sleep(Duration::from_secs(3)).await;
+
+
+    // Send a new followup message
+    let fu = ctx.clone().create_followup(&
+        WebhookMessage::default().content("This is a follow up!")
+    ).await;
+    
+    // fu is a Result<FollowupMessage, HttpError>
+    if let Ok(follow_up) = fu{
+        // Wait another three
+        task::sleep(Duration::from_secs(3)).await;
+        
+        // Finally, delete it
+        if let Err(_) = follow_up.delete_message().await{
+            eprintln!("ERROR")
+        }
+    }
+    
+    // Mind you: The return value is the INITIAL RESPONSE. What is returned here is sent directly to Discord
+    return ctx.respond()
+        .message("I was summoned?")
+        .finish();
+}
+
+// The lib uses actix-web
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    
+    let mut handle = InteractionHandler::new(PUB_KEY);
+    
+    
+    handle.add_command("summon", test);
+ 
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
+    let cert_chain = certs(cert_file).unwrap();
+    let mut keys = pkcs8_private_keys(key_file).unwrap();
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
+
+    return handle.run_ssl(config).await;
+    
+}
+
