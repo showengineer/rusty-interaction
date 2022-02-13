ef884822d3f575ab89dd27ce6a3af422de7932f3
Hugo Woesthuis <hugo@woesthuis.eu>
2022-02-13 13:23:16 +0100

    Better error name for hex conversion errors

--- src/security.rs
+++ src/security.rs
@@ -22,7 +22,7 @@ pub fn verify_discord_message(
     body: &str,
 ) -> Result<(), ValidationError> {
     let signature_bytes = hex::decode(signature)
-        .map_err(|_| ValidationError::KeyConversionError { name: "Signature" })?;
+        .map_err(|_| ValidationError::KeyConversionError { name: "Hex conversion error" })?;
 
     let signature = Signature::from_bytes(signature_bytes.as_slice()).map_err(|_| {
         ValidationError::KeyConversionError {
