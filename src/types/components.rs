9d19c7d9fc5b3454879bbc6925f55c69147ba27a
Hugo Woesthuis <hugow2011@outlook.com>
2022-07-21 21:31:04 +0200

    Generalize Styles
    Textboxes use the same field for styling

--- src/types/components.rs
+++ src/types/components.rs
@@ -1,3 +1,4 @@
+//use std::default::default;
 #[cfg(feature = "builder")]
 use std::error;
 #[cfg(feature = "builder")]
@@ -20,7 +21,7 @@ use serde_with::*;
 pub struct MessageComponent {
     /// Type of component
     r#type: ComponentType,
-    style: Option<ComponentButtonStyle>,
+    style: Option<ComponentStyle>,
     label: Option<String>,
     emoji: Option<PartialEmoji>,
     custom_id: Option<String>,
@@ -183,9 +184,13 @@ impl Default for ComponentButton {
 #[cfg(feature = "builder")]
 impl From<ComponentButton> for MessageComponent {
     fn from(t: ComponentButton) -> Self {
+        let mut a: Option<u8> = None;
+        if let Some(b) = t.style{
+            a = Some(b.into());
+        }
         MessageComponent {
             r#type: ComponentType::Button,
-            style: t.style,
+            style: a,
             label: t.label,
             emoji: t.emoji,
             custom_id: t.custom_id,
@@ -196,6 +201,8 @@ impl From<ComponentButton> for MessageComponent {
     }
 }
 
+/// Alias for generic styling
+pub type ComponentStyle = u8;
 #[derive(Clone, Serialize_repr, Deserialize_repr, PartialEq, Debug)]
 #[repr(u8)]
 #[non_exhaustive]
@@ -211,6 +218,12 @@ pub enum ComponentButtonStyle {
     Danger = 4,
     /// grey with outgoing link icon
     Link = 5,
+}
+
+impl From<ComponentButtonStyle> for ComponentStyle{
+    fn from(a: ComponentButtonStyle) -> Self {
+        a as ComponentStyle
+    }
 }
 
 /// Builder for creating a Component Action Row
@@ -231,6 +244,18 @@ impl Builder<MessageComponent> for ComponentRowBuilder {
 
 #[cfg(feature = "builder")]
 impl ComponentRowBuilder {
+    pub fn add_component(mut self, component: impl Into<MessageComponent>) -> Self{
+        match self.obj.components.as_mut() {
+            None => {
+                self.obj.components = Some(vec![component.into()]);
+            }
+            Some(c) => {
+                c.push(component.into());
+            }
+        }
+        self
+    }
+
     /// Add a button
     pub fn add_button(mut self, button: ComponentButton) -> Self {
         match self.obj.components.as_mut() {
@@ -252,6 +277,18 @@ impl ComponentRowBuilder {
             }
             Some(c) => {
                 c.push(menu.into());
+            }
+        }
+        self
+    }
+    /// Add a textbox to the row
+    pub fn add_textbox(mut self, textbox: ComponentTextBox) -> Self{
+        match self.obj.components.as_mut(){
+            None => {
+                self.obj.components = Some(vec![textbox.into()]);
+            }
+            Some(c) =>{
+                c.push(textbox.into());
             }
         }
         self
@@ -495,21 +532,40 @@ impl Builder<ComponentSelectMenu> for ComponentSelectMenuBuilder {
     }
 }
 
+
+#[derive(Clone, Debug, Default)]
+pub enum ComponentTextBoxStyle {
+    #[default] Short = 1, 
+    Paragraph = 2,
+}
+
+impl From<ComponentTextBoxStyle> for ComponentStyle{
+    fn from(a: ComponentTextBoxStyle) -> Self {
+        a as ComponentStyle
+    }
+}
+
 #[derive(Clone, Debug, Default)]
 /// A textbox component, where users can put text in
 pub struct ComponentTextBox {
     placeholder: Option<String>,
-    label: Option<String>,
+    custom_id: String,
+    label: String,
+    style: ComponentTextBoxStyle,
     min_length: Option<u16>,
     max_length: Option<u16>,
     required: Option<bool>,
 }
 
+
+
 impl From<ComponentTextBox> for MessageComponent {
     fn from(t: ComponentTextBox) -> Self {
         MessageComponent {
             r#type: ComponentType::TextInput,
-            label: t.label,
+            label: Some(t.label),
+            custom_id: Some(t.custom_id),
+            style: Some(t.style.into()),
             min_length: t.min_length,
             max_length: t.max_length,
             required: t.required,
@@ -527,6 +583,21 @@ pub struct ComponentTextBoxBuilder {
 
 #[cfg(feature = "builder")]
 impl ComponentTextBoxBuilder {
+
+    /// Sets the custom ID of this text thing. **MANDATORY**
+    pub fn custom_id(mut self, id: impl Into<String>) -> Self{
+        let pid = id.into();
+
+        self.obj.custom_id = pid;
+        self
+    }
+
+    // Set the textbox label. **MANDATORY**
+    pub fn label(mut self, label: impl Into<String>) -> Self{
+        self.obj.label = label.into();
+        self
+    }
+
     /// Sets the placeholder
     pub fn placeholder(mut self, placeholder: impl Into<String>) -> Self {
         let placeholder = placeholder.into();
@@ -569,6 +640,13 @@ impl Builder<MessageComponent> for ComponentTextBoxBuilder {
     type Error = ComponentTextBoxBuilderError;
 
     fn build(self) -> Result<MessageComponent, Self::Error> {
+
+        if self.obj.custom_id.is_empty() {
+            return Err(ComponentTextBoxBuilderError::MissingCustomId);
+        }
+        if self.obj.label.is_empty(){
+            return Err(ComponentTextBoxBuilderError::MissingLabel);
+        }
         if let Some(ml) = self.obj.min_length.as_ref() {
             if ml > &4000 {
                 return Err(ComponentTextBoxBuilderError::MinimumLengthTooHigh);
@@ -595,6 +673,10 @@ impl Builder<MessageComponent> for ComponentTextBoxBuilder {
 #[derive(Clone, Debug)]
 /// Errors that arise when building the textbox component.
 pub enum ComponentTextBoxBuilderError {
+    /// The required custom id isn't set!
+    MissingCustomId,
+    /// Missing a label
+    MissingLabel,
     /// The minimum length is set too high (> 4000)
     MinimumLengthTooHigh,
     /// The maximum length is set too high (> 4000)
@@ -623,6 +705,12 @@ impl Display for ComponentTextBoxBuilderError {
             }
             ComponentTextBoxBuilderError::MinimumGreaterThanMaximum => {
                 write!(f, "The minimum length is greater than the maximum length")
+            }
+            ComponentTextBoxBuilderError::MissingCustomId => {
+                write!(f, "The required custom id has not been set for this text box")
+            }
+            ComponentTextBoxBuilderError::MissingLabel => {
+                write!(f, "The required label is not set!")
             }
         }
     }
